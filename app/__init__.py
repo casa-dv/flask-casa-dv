6c6a83a4cf77a085936d0c3013c82e10881f224e
Tom Russell <tomalrussell@gmail.com>
2016-05-25 11:10:38 +0100

    proxy api requests

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,153 @@
-from flask import Flask
+from flask import Flask, request, make_response, render_template
+
+import requests
+import json
+from datetime import datetime, timedelta
+
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+TFL_APP_ID = os.environ.get("TFL_APP_ID")
+TFL_APP_KEY = os.environ.get("TFL_APP_KEY")
+EVENTBRITE_TOKEN = os.environ.get("EVENTBRITE_TOKEN")
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello!"
+	return render_template("index.html")
 
+@app.route("/tfl/<path:api_path>")
+def tfl(api_path):
+	base_url = "https://api.tfl.gov.uk/"
+	args = dict(request.args)
+	args['app_key'] = TFL_APP_KEY
+	args['app_id']  = TFL_APP_ID
+	r = requests.get(base_url+api_path, params = args)
+
+	if r.status_code == requests.codes.ok:
+		resp = make_response(r.text, 200)
+		resp.headers['Content-Type'] = r.headers.get('content-type')
+		return resp
+	else:
+		resp = make_response(r.text, 404)
+		return resp
+
+@app.route("/eventbrite_raw/<path:api_path>")
+def eventbrite_events(api_path):
+	# expect full api path and params (except token)
+	# /events/search/
+	# ?location.latitude=51.5218991&location.longitude=-0.1381519&location.within=1km&expand=category,ticket_classes,venue
+	# &start_date.range_start=2016-05-24T00:00:00&start_date.range_end=2016-05-30T23:59:00
+	base_url = "https://www.eventbriteapi.com/v3/"
+	headers = {
+		"Authorization": "Bearer "+EVENTBRITE_TOKEN,
+	}
+	r = requests.get(base_url+api_path, params = request.args, headers = headers)
+
+	if r.status_code == requests.codes.ok:
+		resp = make_response(r.text, 200)
+		resp.headers['Content-Type'] = r.headers.get('content-type')
+		return resp
+	else:
+		resp = make_response(r.text, r.status_code)
+		resp.headers['Content-Type'] = r.headers.get('content-type')
+		return resp
+
+@app.route("/eventbrite")
+def eventbrite_treated():
+	# expect lat and lon, eg:
+	# ?lat=51.5218991&lon=-0.1381519
+	base_url = "https://www.eventbriteapi.com/v3/events/search/"
+	headers = {
+		"Authorization": "Bearer "+EVENTBRITE_TOKEN,
+	}
+	args = {
+		"location.latitude": request.args.get("lat"),
+		"location.longitude": request.args.get("lon"),
+		"location.within": "1km",
+		"start_date.range_start": datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
+		"start_date.range_end": (datetime.now()+timedelta(days=7)).strftime("%Y-%m-%dT%H:%M:%S"), #"2016-03-30T21:00:00",
+		# TODO remove category when finished recode_eventbrite_category
+		"expand": "category,ticket_classes,venue"
+	}
+
+	r = requests.get(base_url, params = args, headers = headers)
+
+	if r.status_code == requests.codes.ok:
+		body = process_events_json(r.json())
+		resp = make_response(body, 200)
+		resp.headers['Content-Type'] = r.headers.get('content-type')
+		return resp
+	else:
+		resp = make_response(r.text, r.status_code)
+		resp.headers['Content-Type'] = r.headers.get('content-type')
+		return resp
+
+def process_events_json(data):
+	if "events" in data:
+		events = []
+		for e in data['events']:
+
+			# check all free
+			free = [t['free'] for t in e['ticket_classes']]
+
+			# check all costs
+			if any(free):
+				costs = [0] # set up a zero cost if any are free
+			else:
+				costs = []
+
+			for t in e['ticket_classes']:
+				if "cost" in t:
+					costs.append(t['cost']['value']) # value in pence
+
+			if len(costs) == 0:
+				costs = [0]
+
+			event = {
+				"properties": {
+					"name": e['name']['html'],
+					# "description": e['description']['text'],
+					"url": e['url'],
+					"category": recode_eventbrite_category(e['category_id']),
+					"start": e['start']['local'],
+					"end": e['end']['local'],
+					"free": any(free),
+					"min": min(costs),
+					"max": max(costs)
+				},
+				"geometry": {
+					"coordinates": [
+						float(e['venue']['longitude']),
+						float(e['venue']['latitude'])
+					],
+					"type": "Point"
+				},
+				"type": "Feature"
+			}
+
+			# for debugging, until finished with recode_eventbrite_category
+			if e['category']:
+				event['properties']['e_category'] = e['category']['name']
+
+			events.append(event)
+		return json.dumps(events)
+
+def recode_eventbrite_category(id):
+	# fill this dict with encoding eg "999" (originally Cheese): "Food & Drink"
+	cats = {
+		"110": "Food & Drink"
+	}
+	if id in cats:
+		return cats[id]
+	else:
+		return "Other"
+
+app.debug = True
 if __name__ == "__main__":
     app.run()
---
+++ app/static/main.js
@@ -0,0 +1,88 @@
+var map = L.map('map');
+var lon = -0.125984;
+var lat = 51.501603;
+map.setView([lat, lon], 15);
+
+var layer = Tangram.leafletLayer({
+		scene: "static/daynight.yaml",
+		preUpdate: preUpdate,
+		postUpdate: postUpdate,
+		attribution: '<a href="https://mapzen.com/tangram">Tangram</a> | &copy; OSM contributors | <a href="https://mapzen.com/">Mapzen</a>'
+});
+
+var scene = layer.scene;
+
+layer.on('init', function() {
+	// everything's good, carry on
+	window.addEventListener('resize', resizeMap);
+	resizeMap();
+});
+
+// Resize map to window
+function resizeMap() {
+	document.getElementById('map').style.width = window.innerWidth + 'px';
+	document.getElementById('map').style.height = window.innerHeight + 'px';
+	map.invalidateSize(false);
+}
+
+layer.on('error', function(error) {
+	// something went wrong
+	var noticeTxt, errorEL = document.createElement('div');
+	errorEL.setAttribute("class", "error-msg");
+		 // WebGL not supported (or at least didn't initialize properly!)
+	if (layer.scene && !layer.scene.gl) {
+		noticeTxt = document.createTextNode("Your browser doesn't support WebGL. Please try with recent Firefox or Chrome, Tangram is totally worth it.");
+		errorEL.appendChild(noticeTxt);
+	 }
+	 // Something else went wrong, generic error message
+	 else {
+		noticeTxt = document.createTextNode("We are sorry, but something went wrong, please try later.");
+		errorEL.appendChild(noticeTxt);
+	 }
+	 document.body.appendChild(errorEL);
+});
+
+layer.addTo(map);
+
+
+function preUpdate(will_render) {
+		if (!will_render) {
+				return;
+		}
+		// daycycle();
+}
+
+function postUpdate() {
+}
+
+function daycycle() {
+		d = new Date();
+		t = d.getTime()/10000;
+
+		x = Math.sin(t);
+		y = Math.sin(t+(3.14159/2)); // 1/4 offset
+		// z = Math.sin(t+(3.14159)); // 1/2 offset
+
+		// scene.view.camera.axis = {x: x, y: y};
+
+		// offset blue and red for sunset and moonlight effect
+		// B = x + Math.abs(Math.sin(t+(3.14159*0.5)))/4;
+		// R = y + Math.abs(Math.sin(t*2))/4;
+
+		// scene.lights.sun.diffuse = [R, y, B, 1];
+		// scene.lights.sun.direction = [x, 1, -0.5];
+
+		px = Math.min(x, 0); // positive x
+		py = Math.min(y, 0); // positive y
+		// light up the roads at night
+		scene.styles.roads.material.emission.amount = [-py, -py, -py, 1];
+		// turn water black at night
+		scene.styles.water.material.ambient.amount = [py+1, py+1, py+1, 1];
+		scene.styles.water.material.diffuse.amount = [py+1, py+1, py+1, 1];
+
+		// turn up buildings' ambient response at night
+		// ba = -py*0.75+0.75;
+		// scene.styles.buildings.material.ambient.amount = [ba, ba, ba, 1];
+
+		scene.animated = true;
+}
